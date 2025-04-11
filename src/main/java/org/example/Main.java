c4e39b07fb5948f7f73eeeee1cdde2806c6b48ab
Kekkuozzo <francescopistone02@gmail.com>
2025-04-11 16:39:41 +0200

    Giacomino

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
+public class Main {
+    public static void main(String[] args) {
+
+        }
+    }
+
+
