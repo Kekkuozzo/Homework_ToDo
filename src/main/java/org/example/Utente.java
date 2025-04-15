1a52f846dd4935e899b3854e90c8aa2be76fbe87
Kekkuozzo <francescopistone02@gmail.com>
2025-04-15 17:54:47 +0200

    Codice ToDo

--- src/main/java/org/example/Utente.java
+++ src/main/java/org/example/Utente.java
@@ -1,21 +1,5 @@
 package org.example;
-
-import java.util.Scanner;
-
 public class Utente {
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);;
-        System.out.println("inserire email");
-        String email = scanner.nextLine();
-        System.out.println("la tua mail è " + email);
-
-
-
-        System.out.println("inserire password");
-        String password = scanner.nextLine();
-        System.out.println("la tua password è " + password);
-        scanner.close();
-
-}
+    private String email;
+    private String password;
 }
